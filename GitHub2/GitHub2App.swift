1b48041be6a9326d27258f046af8272fac782d36
Rahaf-222 <Rahaf.zafer55@gmail.com>
2022-11-02 10:38:34 +0300

    Initial Commit

---
+++ GitHub2/GitHub2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub2App.swift
+//  GitHub2
+//
+//  Created by Rahaf Alahehri on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct GitHub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
